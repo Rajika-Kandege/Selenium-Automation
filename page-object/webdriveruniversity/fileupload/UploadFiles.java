9154aea8f4d012de9e38eafc2a8df771716ee5a3
Rajika-Kandege <54842482+Rajika-Kandege@users.noreply.github.com>
2024-04-23 11:52:10 +0800

    Update UploadFiles.java

--- page-object/webdriveruniversity/fileupload/UploadFiles.java
+++ page-object/webdriveruniversity/fileupload/UploadFiles.java
@@ -3,9 +3,12 @@ package org.example.pages.webdriveruniversity.fileupload;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+import java.util.logging.Logger;
+
 public class UploadFiles {
 
   private WebDriver driver;
+  Logger logger = Logger.getLogger(UploadFiles.class.getName());
 
   public UploadFiles (WebDriver driver) {
     this.driver = driver;
@@ -20,6 +23,7 @@ public class UploadFiles {
    */
   public UploadFiles navigateToPage () {
     driver.navigate().to("https://webdriveruniversity.com/File-Upload/index.html");
+    logger.info(driver.getCurrentUrl());
     return this;
   }
 
@@ -30,7 +34,19 @@ public class UploadFiles {
    */
   public UploadFiles submitWithoutFile () {
     driver.findElement(submitButton).submit();
+    logger.info("Click submit button");
     return this;
 
+  }
+
+  /**
+   * Get the page title
+   * @return UploadFiles
+   */
+
+  public UploadFiles getPageTitle(){
+   String title =  driver.getTitle();
+    System.out.println(title);
+    return this;
   }
 }
