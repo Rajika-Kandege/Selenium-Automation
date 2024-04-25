41009f5b5b7935ab4e1068e461e71b59f800d3af
Rajika-Kandege <54842482+Rajika-Kandege@users.noreply.github.com>
2024-04-25 12:36:14 +0800

    Update Accordian.java

--- page-object/accoradance/Accordian.java
+++ page-object/accoradance/Accordian.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -21,6 +22,26 @@ public class Accordian {
   By manualTestingLocator = By.id("manual-testing-accordion");
   String url = "https://webdriveruniversity.com/Accordion/index.html";
   By manualTestingDescriptionLocator = By.id("manual-testing-description");
+  By firstTextBox = By.id("text-appear-box");
+
+  /**
+   * Wait until text appears
+   *
+   * @return Accordian
+   */
+  public Accordian waitUntilTextAppears () {
+
+    FluentWait<WebDriver> wait = new FluentWait<>(driver).
+            withTimeout(Duration.ofSeconds(15)).
+            pollingEvery(Duration.ofSeconds(2)).
+            ignoring(Exception.class);
+    wait.until(ExpectedConditions.
+            not(ExpectedConditions.textToBePresentInElement
+                    (driver.findElement(firstTextBox), "LOADING.. PLEASE WAIT..")));
+    System.out.println(driver.findElement(firstTextBox).getText());
+    logger.info("Waited until text appears complete.");
+    return this;
+  }
 
   /**
    * Navigate into the web page
